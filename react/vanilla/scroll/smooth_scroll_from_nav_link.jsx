adbf38e9a6d858047f3f0b91fea5010f4e2f3183
Selva Ganesh M <selvadev2k@gmail.com>
2023-01-01 02:39:02 +0530

    Initial commit

---
+++ react/vanilla/scroll/smooth_scroll_from_nav_link.jsx
@@ -0,0 +1,17 @@
+// npm install react-scroll;
+
+// import {Link} from "react-scroll"
+
+{
+  /* <Link
+  activeClass="active"
+  to="test1"
+  spy={true}
+  smooth={true}
+  offset={50}
+  duration={500}
+  onSetActive={this.handleSetActive}
+>
+  Test 1
+</Link>; */
+}
